253353edb234ab33f9edd1129130dbe75f787107
Yaroslav <yaros6666@yandex.ru>
2023-03-06 14:52:19 +0300

    done correct method data_bases

--- model/ReadWriteBD.py
+++ model/ReadWriteBD.py
@@ -5,7 +5,7 @@ from model.Exception import ExceptionReqDB, ExceptionConnectToDB
 
 
 class ReadWriteBD:
-    def read_bd(self, command):
+    def read_write_bd(self, command, flag):
         try:
             connection = pymysql.connect(
                 host=host,
@@ -19,35 +19,13 @@ class ReadWriteBD:
             try:
                 with connection.cursor() as cursor:
                     cursor.execute(command)
-                    rows = cursor.fetchall()
+                    if flag == 'r':
+                        rows = cursor.fetchall()
+                        return rows
+                    elif flag == 'w':
+                        connection.commit()
 
-            except Exception:
-                raise ExceptionReqDB()
 
-            finally:
-                connection.close()
-            return rows
-        except ExceptionReqDB:
-            print(ExceptionReqDB.description)
-            exit()
-        except Exception:
-            raise ExceptionConnectToDB("Error connecting to the database")
-
-    def write_bd(self, command):
-        try:
-            connection = pymysql.connect(
-                host=host,
-                port=3306,
-                user=user,
-                password=password,
-                database=db_name,
-                cursorclass=pymysql.cursors.DictCursor
-            )
-
-            try:
-                with connection.cursor() as cursor:
-                    cursor.execute(command)
-                    connection.commit()
             except Exception:
                 raise ExceptionReqDB()
 
@@ -60,6 +38,33 @@ class ReadWriteBD:
         except Exception:
             raise ExceptionConnectToDB("Error connecting to the database")
 
+    # def write_bd(self, command):
+    #     try:
+    #         connection = pymysql.connect(
+    #             host=host,
+    #             port=3306,
+    #             user=user,
+    #             password=password,
+    #             database=db_name,
+    #             cursorclass=pymysql.cursors.DictCursor
+    #         )
+    #
+    #         try:
+    #             with connection.cursor() as cursor:
+    #                 cursor.execute(command)
+    #                 connection.commit()
+    #         except Exception:
+    #             raise ExceptionReqDB()
+    #
+    #         finally:
+    #             connection.close()
+    #
+    #     except ExceptionReqDB:
+    #         print(ExceptionReqDB.description)
+    #         exit()
+    #     except Exception:
+    #         raise ExceptionConnectToDB("Error connecting to the database")
+    #
 
 
 
--- model/Shop.py
+++ model/Shop.py
@@ -12,9 +12,6 @@ class Shop:
     ReadWriteBD = ReadWriteBD()
     view = ViewConsole()
 
-    # def __init__(self):
-    #     self.list_toy = self.ReadWriteBD.read_bd()
-
     def add_toy_inDB(self):
         new_toy = []
         name = Check().check_name_toy()
@@ -23,27 +20,27 @@ class Shop:
         for i in (name, amount_toy, frequency):
             new_toy.append(i)
 
-        self.ReadWriteBD.write_bd(f"INSERT INTO `toys_in_shop` (title_toy, amount, frequency) "
-                                    f"VALUES ('{new_toy[0]}', '{new_toy[1]}', '{new_toy[2]}');")
+        self.ReadWriteBD.read_write_bd(f"INSERT INTO `toys_in_shop` (title_toy, amount, frequency) "
+                                    f"VALUES ('{new_toy[0]}', '{new_toy[1]}', '{new_toy[2]}');", 'w')
 
         self.view.output_console("Игрушка успешно добавлена в базу", True)
 
     def get_list_toy(self):
-        self.list_toy = self.ReadWriteBD.read_bd("SELECT * FROM `toys_in_shop`")
+        self.list_toy = self.ReadWriteBD.read_write_bd("SELECT * FROM `toys_in_shop`", 'r')
 
     def get_list_for_issue(self):
-        self.list_for_issue = self.ReadWriteBD.read_bd("SELECT id, title_toy, date_of_winning "
+        self.list_for_issue = self.ReadWriteBD.read_write_bd("SELECT id, title_toy, date_of_winning "
                                                        "FROM `toys_for_delivery`"
-                                                       "WHERE toy_issued = 0")
+                                                       "WHERE toy_issued = 0", 'r')
 
     def give_toy(self):
         self.get_list_for_issue()
         if not len(self.list_for_issue) ==0:
             id_for_give = self.list_for_issue[0]['id']
-            self.ReadWriteBD.write_bd(f"UPDATE `toys_for_delivery` "
+            self.ReadWriteBD.read_write_bd(f"UPDATE `toys_for_delivery` "
                                       f"SET toy_issued = '1',"
                                       f"date_issue = '{datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')}'"
-                                      f"WHERE id = '{id_for_give}';")
+                                      f"WHERE id = '{id_for_give}';", 'w')
             self.view.output_console("Игрушка успешно выдана", True)
         else:
             self.view.output_console('Нет игрушек для выдачи', False)
